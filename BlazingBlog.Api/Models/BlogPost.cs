957fb6ef6265b0210ee6769711a1187411b73576
Cykotech <cykotech.development@yahoo.com>
2025-03-29 13:07:11 -0400

    Create BlogPost Model

---
+++ BlazingBlog.Api/Models/BlogPost.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazingBlog.Api.Models;
+
+// Database Entity
+public class BlogPost
+{
+    public int Id { get; init; }
+
+    [Required] public string Title { get; set; }
+
+    [Required] public string Content { get; set; }
+
+    public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+
+    public List<DateTime> ModifiedAt { get; } = [];
+    
+    public DateTime DeletedAt { get; set; }
+
+    public BlogPost(string title, string content)
+    {
+        Title = title;
+        Content = content;
+    }
+}

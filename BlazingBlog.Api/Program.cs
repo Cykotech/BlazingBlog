13f53bcce37f6e577a034bacad3284be11d21f61
Cykotech <cykotech.development@yahoo.com>
2025-04-03 21:02:53 -0400

    Add database connection

--- BlazingBlog.Api/Program.cs
+++ BlazingBlog.Api/Program.cs
@@ -5,9 +5,11 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
         // Add services to the container.
         builder.Services.AddAuthorization();
+        builder.Services.AddDbContext<BlogDbContext>(options => options.UseSqlServer(connectionString));
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
